de68842beaa349f19d2293e977454049117103b5
Petr Čechura <p.cechura@gmail.com>
2013-02-19 23:20:58 +0100

    Add stored playlist queries

--- Wmpc.Mpd/PlaylistProvider.cs
+++ Wmpc.Mpd/PlaylistProvider.cs
@@ -63,55 +63,133 @@ plchangesposid {VERSION}
             return this.client.SendCommand(command);
         }
 
-        public Status QueryClear() {
-            return new Status(this.client.SendCommand(MPD_CLEAR));
+        public Response QueryClear() {
+            return this.client.SendCommand(MPD_CLEAR);
         }
 
-        public Stats QueryDelete(int start, int end) {
+        public Response QueryDelete(int start, int end) {
             Command command = new Command(MPD_DELETE, start.ToString() + ":" + end.ToString());
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
-        public Stats QueryDelete(int pos) {
+        public Response QueryDelete(int pos) {
             Command command = new Command(MPD_DELETE, pos);
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
-        public Stats QueryDeleteId(int songid) {
+        public Response QueryDeleteId(int songid) {
             Command command = new Command(MPD_DELETEID, songid);
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
-        public Stats QueryPrio(byte priority, int start, int end) {
+        public Response QueryPrio(byte priority, int start, int end) {
             Command command = new Command(MPD_PRIO, (int)priority, start.ToString() + ":" + end.ToString());
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
-        public Stats QueryPrioId(byte priority, int id) {
+        public Response QueryPrioId(byte priority, int id) {
             Command command = new Command(MPD_PRIOID, (int)priority, id);
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
-        public Stats QueryShuffle(int start, int end) {
+        public Response QueryShuffle(int start, int end) {
             Command command = new Command(MPD_SHUFFLE, start.ToString() + ":" + end.ToString());
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
-        public Stats QueryShuffle() {
-            return new Stats(this.client.SendCommand(MPD_SHUFFLE));
+        public Response QueryShuffle() {
+            return this.client.SendCommand(MPD_SHUFFLE);
         }
 
-        public Stats QuerySwap(int song1, int song2) {
+        public Response QuerySwap(int song1, int song2) {
             Command command = new Command(MPD_SWAP, song1, song2);
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
 
-        public Stats QuerySwapId(int id1, int id2) {
+        public Response QuerySwapId(int id1, int id2) {
             Command command = new Command(MPD_SWAPID, id1, id2);
-            return new Stats(this.client.SendCommand(command));
+            return this.client.SendCommand(command);
         }
 
+
+
+        const string MPD_LISTPLAYLIST = "listplaylist";
+        const string MPD_LISTPLAYLISTINFO = "listplaylistinfo";
+        const string MPD_LISTPLAYLISTS = "listplaylists";
+        const string MPD_LOAD = "load";
+        const string MPD_PLAYLISTADD = "playlistadd";
+        const string MPD_PLAYLISTCLEAR = "playlistclear";
+        const string MPD_PLAYLISTDELETE = "playlistdelete";
+        const string MPD_PLAYLISTMOVE = "playlistmove";
+        const string MPD_RENAME = "rename";
+        const string MPD_RM = "rm";
+        const string MPD_SAVE = "save";
+
+        public Response QueryListPlaylist(string name) {
+            Command command = new Command(MPD_LISTPLAYLIST, name);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryListPlaylistInfo(string name) {
+            Command command = new Command(MPD_LISTPLAYLISTINFO, name);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryListPlaylist(string name) {
+            Command command = new Command(MPD_LISTPLAYLIST, name);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryListPlaylists() {
+            Command command = new Command(MPD_LISTPLAYLISTS);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryLoad(string name) {
+            Command command = new Command(MPD_LOAD, name);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryLoad(string name, int start, int end) {
+            Command command = new Command(MPD_LOAD, name, start.ToString() + ":" + end.ToString());
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryPlaylistAdd(string name, string uri) {
+            Command command = new Command(MPD_PLAYLISTADD, name, uri);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryPlaylistClear(string name) {
+            Command command = new Command(MPD_PLAYLISTCLEAR, name);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryPlaylistDelete(string name, int songpos) {
+            Command command = new Command(MPD_PLAYLISTDELETE, name, songpos);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryPlaylistMove(string name, int songid, int songpos) {
+            Command command = new Command(MPD_PLAYLISTMOVE, name, songid.ToString() + " " + songpos.ToString());
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryRename(string name, string newName) {
+            Command command = new Command(MPD_PLAYLISTCLEAR, name, newName);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QueryRm(string name) {
+            Command command = new Command(MPD_RM, name);
+            return this.client.SendCommand(command);
+        }
+
+        public Response QuerySave(string name) {
+            Command command = new Command(MPD_SAVE, name);
+            return this.client.SendCommand(command);
+        }
 
     }
 }
